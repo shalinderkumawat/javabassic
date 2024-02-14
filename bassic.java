3bbb44e9b01a32d047211432b5abaed0007fb1d8
shalinder kumawat <kumarkumawatkumarkumawat@gmail.com>
2024-02-14 17:14:39 +0530

    java

---
+++ bassic.java
@@ -0,0 +1,153 @@
+ Q1
+   print the hello world in java language
+
+
+   public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("hello world");
+
+    }
+}
+
+
+Q2
+   print the same line in your code
+
+
+   public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("hello world");
+        System.out.print("hello world");
+    }
+}
+
+Q3
+   print the next new line
+
+
+   public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+        System.out.print("hello world");
+    }
+}
+
+System.out.println=println  print the next line
+
+
+Q4 print the hello world in java language use "\n"
+
+public class Main
+{
+    public static void main(String [] args)
+    {
+        System.out.print("hello woeld \n hello world");
+    }
+}
+
+Q5  print the int type value in java language
+
+  public class Main
+{
+    public static void main(String[] args)
+    {
+        //int type value 
+        int inttype=3;
+        System.out.print(inttype);
+    }
+}
+
+
+Q6 add the two int type value in java language
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //two int type value
+        int fristvalue=12;
+        int secondvalue=12;
+        System.out.println(fristvalue);
+       System.out.print(secondvalue);
+    }
+}
+
+Q7 add the two number without threed int value in java language
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int addnumber1=123;
+        int addnumber2=12;
+        System.out.print(addnumber2+addnumber1);
+    }
+}
+
+Q8 add the two number with threed int value in java language
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int add1=12;
+        int add2=10;
+        int sum=add1+add2;
+        System.out.print(sum);
+    }
+}
+
+Q9 multiply in java language
+
+  public class Main
+{
+    public static void main(String[] args)
+    {
+        int number1=12,number2=12;
+        int multiply=number1*number2;
+        System.out.print(multiply);
+    }
+}
+
+Q10 add,multiply,divison,modlur,subtraction all in one code in java language
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+	int number = 10, number2 = 10;
+
+	int add = number + number2;
+	  System.out.println (add);
+	int multiply = number * number2;
+	  System.out.println (multiply);
+	int divison = number / number2;
+	  System.out.println (divison);
+	int subtraction = number - number2;
+	  System.out.println (subtraction);
+	int modlur = number % number2;
+	  System.out.println (modlur);
+  }
+}
+
+Q11 a*b
+    ___ int value in java language
+    a-b
+
+
+    public class Main
+{
+    public static void main(String[] args)
+    {
+        int number1=3,number2=2;
+        System.out.print((number1*number2)/(number1-number2));
+    }
+}
+
+
